3d7d53823f5a1917060ab244162ee59a9cf94519
Anushka Sharma <anushkaece007@gmail.com>
2021-05-31 20:54:34 +0530

    Add files via upload

---
+++ multiset.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+    multiset<int>s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(3);
+    s.insert(3);
+    s.insert(3);
+    s.insert(3);
+    s.insert(4);
+    for(auto i:s)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<s.size()<<"\n";
+    s.erase(3);
+    for(auto i:s)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
