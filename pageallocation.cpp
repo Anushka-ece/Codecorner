9ff02fe390f461e6ee1dfb405513f386e3646759
Anushka Sharma <anushkaece007@gmail.com>
2021-05-31 20:55:44 +0530

    Add files via upload

---
+++ pageallocation.cpp
@@ -0,0 +1,63 @@
+//BINARY SEARCH CHALLENGE 2
+#include<iostream>
+#include<climits>
+using namespace std;
+
+bool ispossible(int arr[],int n,int m,int min)
+{
+int studentsrequired=1,sum=0;//studentsrequired
+for(int i=0;i<n;i++)
+{
+    if(arr[i]>min)
+    {
+        return false;
+    }if(sum+arr[i]>min)
+    {
+        studentsrequired++;
+        sum=arr[i];
+        if(studentsrequired>m)
+        return false;
+    }
+}
+else
+{
+    sum+=arr[i];
+}
+return true;
+}
+
+int allocateminimum(int arr[],int n,int m)//n means n different books and m means m different students
+{
+int sum=0;//sum of array elements
+if(n<m)
+{
+    return -1;
+}
+for(int i=0;i<n;i++)
+{
+    sum+=arr[i];
+}
+int start=0,end=sum,ans=INT_MAX;//we have taken maximum sum value equal to the maximum number of books taken
+while(start<=end)
+{
+    int mid=(start+end)/2;
+    if(ispossible(arr,n,m,mid))
+    {
+        ans=min(ans,mid);
+        end=mid-1;
+    }
+    else{
+        start=mid+1;
+    }
+}
+return ans;
+}
+
+int main()
+{
+    int arr[]={12,34,67,90};
+    int n=4;
+    int m=2;
+    cout<<allocateminimum(arr,n,m);
+    return 0;
+}
