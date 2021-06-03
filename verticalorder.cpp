5bcc3e25c4403a3264603c72a9b09ee418dfffc4
Anushka Sharma <anushkaece007@gmail.com>
2021-06-04 00:14:37 +0530

    Add files via upload

---
+++ verticalorder.cpp
@@ -0,0 +1,48 @@
+//Print vertical order of a binary tree
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+struct Node{
+    int key;
+    Node *left, *right;
+};
+Node* newNode(int key)
+{
+    Node* node=new Node;
+    node->key=key;
+    node->left=node->right=NULL;
+    return node;
+}
+
+void getverticalorder(Node* root,int hdis,map<int,vector<int>>&m)
+{
+if(root==NULL)
+return;
+m[hdis].push_back(root->key);
+getverticalorder(root->left,hdis-1,m);
+getverticalorder(root->right,hdis+1,m);
+}
+
+signed main()
+{Node *root=newNode(10);
+root->left=newNode(7);
+root->right=newNode(4);
+root->left->left=newNode(3);
+root->left->right=newNode(11);
+root->right->left=newNode(14);
+root->right->right=newNode(6);
+map<int,vector<int>>m;//we may have more than one elements at the same horizontal distance
+int hdis=0;
+getverticalorder(root,hdis,m);
+map<int,vector<int>>:: iterator it;
+for(it=m.begin();it!=m.end();it++)
+{
+    for(int i=0;i<(it->second).size();i++)
+    {
+        cout<<(it->second)[i]<<" ";
+    }
+    cout<<endl;
+}
+    return 0;
+}
