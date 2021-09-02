4174ed7ba95a6f2f40e5beb4ec0414104fd1320a
Anushka Sharma <anushkaece007@gmail.com>
2021-09-02 23:29:58 +0530

    Add files via upload

--- BFS.cpp
+++ BFS.cpp
@@ -1,43 +1,41 @@
-#include "bits/stdc++.h"
+#include<iostream>
 using namespace std;
 const int N=1e5+2;
-bool vis[N];//visited array
-vector<int>adj[N];//adjacency list
+bool vis[N];//we have made a visited array
+vector<int>adj[N];//here we have made an adjacency list
 int main()
 {
-    for(int i==0;i<N;i++)
+    for(int i=0;i<N;i++)
     {
-        vis[i]=0;
+        vis[i]=0;//initialized visited array with 0
     }
-    int n,m;//we are taking no. of edges and nodes as input
-    cin>>n>>m;
-int x,y;//these two variables are declared for taking edges as input 
+int n,m;
+cin>>n>>m;//intake of nodes and edges
+//now,we will take variables for taking input of edges
 for(int i=0;i<m;i++)
 {
     cin>>x>>y;
     adj[x].push_back(y);
     adj[y].push_back(x);
 }
-//Till here,our input taking process is complete
-//since we are doing BFS,it is implememted through queue
+//Now,since we know that,for implementing BFS,we use queue data structure
 queue<int>q;
 q.push(1);
-vis[1]=true;//1 has been inserted into this queue and is marked as true
-while(!q.empty())//we will traverse queue till the time it will not become empty
+vis[1]=true;//now at the first step,we will push 1 into queue ,as it is the first node of the tree,and we will mark it as true in the array
+while(!q.empty())//we will keep on popping elements out of the tree,till the time it doesn't get empty
 {
-    int node=q.front();
-    q.pop();
-    cout<<node<<endl;
-    //now one by one we have to visit each node and will mark visited nodes as true
-    vector<int>::iterator it;
-    for(it=adj[node].begin();it!=adj[node].end();it++)
-    {
-        if(!vis[*it])//this is to check if a particular node has been visited or not
-        {
-            vis[*it]=1;
-            q.push(*it);
-        }
-    }
-} 
+int node=q.front();
+q.pop();
+cout<<node<<endl;
+vector<int>::iterator it;
+for(it=adj[node].begin();it!=adj[node].end();it++)
+{
+if(!vis[*it])//this is the condition to check if a particular element has been visited or not
+{
+    vis[*it]=1;
+    q.push(*it);
+}
+}
+}
     return 0;
 }
