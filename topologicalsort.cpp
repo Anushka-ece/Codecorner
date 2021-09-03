0ea912592a57c4a88365b6081f005da7a2d17d05
Anushka Sharma <anushkaece007@gmail.com>
2021-09-03 17:20:06 +0530

    Add files via upload

--- topologicalsort.cpp
+++ topologicalsort.cpp
@@ -1,41 +1,42 @@
-//An ordering of the sequence of ebents is called as topological sort
-#include"bits/stdc++.h"
+#include<iostream>
+#include<vector>
 using namespace std;
-int32_t int main()
+
+int main()
 {
-   int n,m;
-   cin>>n>>m;
-   int cnt=0;
-   vector<vector<int>>adj(n);//we will make an adjacency list here
-   vector<int<indeg(n,0);
-   for(int i=0;i<m;i++)
-   {
-       int x,y;
-       cin>>x>>y;
-       adj[x].push_back(y);
-       indeg[y]++;//here we have to increase number of indegree as well 
-      
-   }
-   queue<int>pq;
-   for(int i=0;i<n;i++)
-   {
-       if(indeg[i]==0)//this shows that if operation is successful ,then in that case element will be pushed
-       {
-           pq.push(i);
-       }
-   }
-   while(!pq.empty())
-   {
-cnt++;
-int x=pq.front();
-pq.pop();
-cout<<x<<" ";
-for(auto it:adj[x])
+    int n,m;
+    cin>>n>>m;//here we are taking nodes and edges as input
+int cnt=0;//here we have made a count variable
+for(int i=0;i<m;i++)
 {
-    indeg[it]--;
-    if(indeg[it]==0)
-    pq.push(it);
+    int u,v;
+    cin>>u>>v;//this is for taking edges as input
+    vector<vector<int>>adj(n);//yhis one is the adjacency list
+    vector<int>indegree(n,0);//this one is another vector where indegree of each vertex is 0 initially
+    //u-->v//this represents that an edge is getting formed from u to v
+    adj[u].push_back(v);
+indegree[v]++;//indegree of a node is increased if there is an incoming edge for it
 }
-   }
-    return 0;
+queue<int>q;
+for(int i=0;i<n;i++)
+{
+    if(indegree[i]==0)//we will check that if indegree of a particular vertex is 0,then we can push this into queue
+    {
+        q.push(i);
+    }
+}
+while(!q.empty())//now we will move till the time q is not empty
+{
+    cnt++;
+    int x=q.front();
+    q.pop();
+    cout<<x<<" ";
+    //x-->v
+    for(auto it:adj[x])
+    {
+        indegree[it--];
+        if(indegree[it]==0)
+        q.push(it);
+    }
+}
 }
